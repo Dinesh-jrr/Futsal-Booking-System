74724a80af00dbf9a2448420da076ea255a2e874
DineshJrr <jrrdinesh2107@gmail.com>
2025-04-17 14:21:43 +0545

    toast applied in signin too and fixed the issue of overlap

--- lib/presentation/auth/pages/signin.dart
+++ lib/presentation/auth/pages/signin.dart
@@ -8,6 +8,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:player/utils/toast_helper.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -26,15 +27,18 @@ class _SignInState extends State<SignIn> {
     final String password = _passwordController.text;
 
     if (email.isEmpty || password.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Please fill in both fields")),
+      showToast(
+        context: context,
+        message: "Please fill in both fields",
+        backgroundColor: Colors.red,
+        icon: Icons.error,
       );
       return;
     }
 
     try {
       final response = await http.post(
-        Uri.parse('http://172.20.10.6:5000/api/users/login'),
+        Uri.parse('http://192.168.1.9:5000/api/users/login'),
         headers: {'Content-Type': 'application/json'},
         body: json.encode({'email': email, 'password': password}),
       );
@@ -48,20 +52,37 @@ class _SignInState extends State<SignIn> {
           await prefs.setString('user_id', responseBody['user']['id']);
           await prefs.setBool('isLoggedIn', true);
 
+          showToast(
+            context: context,
+            message: "Login successful",
+            backgroundColor: Colors.green,
+            icon: Icons.check_circle,
+          );
+          await Future.delayed(const Duration(milliseconds: 500));
+
           Navigator.pushReplacementNamed(context, '/home');
         } else {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text("Login failed: No token found")),
+          showToast(
+            context: context,
+            message: "Login failed: No token found",
+            backgroundColor: Colors.red,
+            icon: Icons.error,
           );
         }
       } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text("Login failed: Invalid credentials")),
+        showToast(
+          context: context,
+          message: "Login failed: Invalid credentials",
+          backgroundColor: Colors.red,
+          icon: Icons.error,
         );
       }
     } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Login failed: Network error")),
+      showToast(
+        context: context,
+        message: "Login failed: Network error",
+        backgroundColor: Colors.red,
+        icon: Icons.error,
       );
     }
   }
@@ -84,10 +105,9 @@ class _SignInState extends State<SignIn> {
       final String googleId = account.id;
       final GoogleSignInAuthentication auth = await account.authentication;
       final String? idToken = auth.idToken;
-      
 
       final response = await http.post(
-        Uri.parse('http://10.22.2.114:5000/api/users/google-login'),
+        Uri.parse('http://192.168.1.9:5000/api/users/google-login'),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({
           'email': email,
@@ -103,10 +123,21 @@ class _SignInState extends State<SignIn> {
         SharedPreferences prefs = await SharedPreferences.getInstance();
         await prefs.setString('auth_token', token);
         await prefs.setBool('isLoggedIn', true);
+
+        showToast(
+          context: context,
+          message: "Google login successful",
+          backgroundColor: Colors.green,
+          icon: Icons.check_circle,
+        );
+
         Navigator.pushReplacementNamed(context, '/home');
       } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Google login failed: ${response.body}")),
+        showToast(
+          context: context,
+          message: "Google login failed: ${response.body}",
+          backgroundColor: Colors.red,
+          icon: Icons.error,
         );
       }
     }
@@ -121,109 +152,114 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(height: 40),
-            Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(16),
-                child: Image.asset(
-                  './assets/images/logo.png',
-                  height: 100,
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Center(
-              child: Text("Welcome !", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-            ),
-            const SizedBox(height: 15),
-            const Center(
-              child: Text("Sign in to your account", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-            ),
-            const SizedBox(height: 20),
-            const Text("Email", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 8),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.email, color: Colors.green),
-                hintText: 'Enter your email',
-                contentPadding: const EdgeInsets.symmetric(vertical: 20),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0), borderSide: const BorderSide(color: Colors.green)),
-              ),
-            ),
-            const SizedBox(height: 25),
-            const Text("Password", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 8),
-            TextField(
-              controller: _passwordController,
-              obscureText: _obscurePassword,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.password, color: Colors.green),
-                hintText: 'Enter your Password',
-                contentPadding: const EdgeInsets.symmetric(vertical: 20),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0), borderSide: const BorderSide(color: AppColors.primary)),
-                suffixIcon: IconButton(
-                  icon: Icon(_obscurePassword ? Icons.visibility_off : Icons.visibility, color: Colors.green),
-                  onPressed: () => setState(() => _obscurePassword = !_obscurePassword),
-                ),
-              ),
-            ),
-            const SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                TextButton(
-                  onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const ForgotPasswordPage())),
-                  child: const Text("Forgot Password?", style: TextStyle(fontSize: 14, color: Colors.red)),
+                const SizedBox(height: 40),
+                Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Image.asset(
+                      './assets/images/logo.png',
+                      height: 100,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Center(
+                  child: Text("Welcome !", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                ),
+                const SizedBox(height: 15),
+                const Center(
+                  child: Text("Sign in to your account", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                ),
+                const SizedBox(height: 20),
+                const Text("Email", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 8),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.email, color: Colors.green),
+                    hintText: 'Enter your email',
+                    contentPadding: const EdgeInsets.symmetric(vertical: 20),
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0), borderSide: const BorderSide(color: Colors.green)),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                const Text("Password", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 8),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: _obscurePassword,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.password, color: Colors.green),
+                    hintText: 'Enter your Password',
+                    contentPadding: const EdgeInsets.symmetric(vertical: 20),
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0), borderSide: const BorderSide(color: AppColors.primary)),
+                    suffixIcon: IconButton(
+                      icon: Icon(_obscurePassword ? Icons.visibility_off : Icons.visibility, color: Colors.green),
+                      onPressed: () => setState(() => _obscurePassword = !_obscurePassword),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const ForgotPasswordPage())),
+                      child: const Text("Forgot Password?", style: TextStyle(fontSize: 14, color: Colors.red)),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: _signIn,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.primary,
+                    padding: const EdgeInsets.symmetric(vertical: 24.0),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                  ),
+                  child: const Text("Sign In", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white)),
+                ),
+                const SizedBox(height: 30),
+                OutlinedButton.icon(
+                  onPressed: handleGoogleLogin,
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(color: Colors.green, width: 2),
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                  ),
+                  icon: Image.asset('assets/icons/google.png', height: 24),
+                  label: const Text("Sign in with Google", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.green)),
+                ),
+                const SizedBox(height: 30),
+                Center(
+                  child: TextButton(
+                    onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUp())),
+                    child: const Text.rich(
+                      TextSpan(
+                        text: "Don't have an account? ",
+                        style: TextStyle(fontSize: 16, color: Colors.black),
+                        children: [
+                          TextSpan(
+                            text: "Sign Up",
+                            style: TextStyle(fontSize: 16, color: AppColors.primary, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: _signIn,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: AppColors.primary,
-                padding: const EdgeInsets.symmetric(vertical: 24.0),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-              ),
-              child: const Text("Sign In", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white)),
-            ),
-            const SizedBox(height: 30),
-            OutlinedButton.icon(
-              onPressed: handleGoogleLogin,
-              style: OutlinedButton.styleFrom(
-                side: const BorderSide(color: Colors.green, width: 2),
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-              ),
-              icon: Image.asset('assets/icons/google.png', height: 24),
-              label: const Text("Sign in with Google", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.green)),
-            ),
-            const SizedBox(height: 30),
-            Center(
-              child: TextButton(
-                onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUp())),
-                child: const Text.rich(
-                  TextSpan(
-                    text: "Don't have an account? ",
-                    style: TextStyle(fontSize: 16, color: Colors.black),
-                    children: [
-                      TextSpan(
-                        text: "Sign Up",
-                        style: TextStyle(fontSize: 16, color: AppColors.primary, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
