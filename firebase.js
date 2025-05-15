63b02d05ab15c873abc89787a82d49185c496fc9
DineshJrr <jrrdinesh2107@gmail.com>
2025-05-15 17:03:14 +0545

    updated fcm part

--- firebase.js
+++ firebase.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const admin = require("firebase-admin");
 // const serviceAccount = require("./config/fcm-service-key.json");
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+// Convert `\\n` to real line breaks for Firebase private_key to work
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
