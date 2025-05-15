52bce61287fb16675b74f0fe434dc99d21b47b6b
DineshJrr <jrrdinesh2107@gmail.com>
2025-05-15 16:56:23 +0545

    fcm setup in railway

--- firebase.js
+++ firebase.js
@@ -1,9 +1,12 @@
 
+require('dotenv').config();
 const admin = require("firebase-admin");
-const serviceAccount = require("./config/fcm-service-key.json");
+// const serviceAccount = require("./config/fcm-service-key.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+console.log("✅ Firebase Admin initialized"); 
 
 module.exports = admin; // ✅ Export the initialized admin instance
